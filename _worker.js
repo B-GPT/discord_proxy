e360eca968d2f89025a1410c1cbcc1181f8b897e
vbarter <yzcaijunjie@gmail.com>
2024-02-19 16:25:05 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,12 +1,21 @@
-export default {
-  async fetch(request, env) {
-    const url = new URL(request.url);
-    url.host = "www.discord.com";
-    return fetch(url, {
-      headers: request.headers,
-      method: request.method,
-      body: request.body
-     // redirect: 'follow'
-    });
-  }
+const API_HOST = "discord.com";
+
+async function handleRequest(request, ctx) {
+  const url = new URL(request.url);
+  const pathname = url.pathname;
+  const search = url.search;
+  const pathWithParams = pathname + search;
+  
+  return forwardRequest(request, pathWithParams);
 }
+
+async function forwardRequest(request, pathWithSearch) {
+  const originRequest = new Request(request);
+  return await fetch(`https://${API_HOST}${pathWithSearch}`, originRequest);
+}
+
+export default {
+  async fetch(request, env, ctx) {
+    return handleRequest(request, ctx);
+  }
+};
